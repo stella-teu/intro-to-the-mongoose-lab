4c25861b5880908b59ba8f1018249699cfdb0991
StellaTeuscher <stella.teuscher@gmail.com>
2025-03-23 19:13:38 -0400

    finished lab

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ import Customer from "./model/customer.js";
 import "dotenv/config";
 
 console.log("Welcome to the CRM.");
-mongoose.connect(process.env.MONGODB_URI);
 
 const createCustomer = async () => {
-  const customerName = prompt("What is their name?");
-  let customerAge = prompt("What is their age?");
+  const customerName = prompt("What is their name?\n");
+  let customerAge = prompt("What is their age?\n");
   customerAge = Number(customerAge);
 
   const customerData = {
@@ -19,29 +18,56 @@ const createCustomer = async () => {
 
   const customer = await Customer.create(customerData);
   console.log("New Customer: " + customer);
+  userAction();
 }
 
 const viewCustomers = async () => {
   const customers = await Customer.find({});
   console.log("All customers: " + customers);
+  userAction();
+}
+
+const updateCustomer = async () => {
+  const customers = await Customer.find({});
+  console.log("Here is a list of all customers: " + customers);
+  console.log("Copy and paste the ID of the customer you'd like to update.");
+  const customerId = prompt();
+  const updatedCustomer = await Customer.findByIdAndUpdate(customerId);
+  const customerName = prompt("What is their new name?\n");
+  let customerAge = prompt("What is their new age?\n");
+  updatedCustomer.name = customerName;
+  updatedCustomer.age = customerAge;
+  console.log("Customer changes.")
+  await updatedCustomer.save();
+  console.log("Customer updated.")
+  userAction();
+}
+
+const deleteCustomer = async () => {
+  const customers = await Customer.find({});
+  console.log("Here is a list of all customers: " + customers);
+  console.log("Copy and paste the ID of the customer you'd like to delete.");
+  const customerId = prompt();
+  const deletedCustomer = await Customer.findByIdAndDelete(customerId);
+  console.log("Deleted customer: " + deletedCustomer);
+  userAction();
 }
 
 const userAction = async () => {
-  console.log("What would you like to do? Type the number of chosen action \n 1. Create a customer. \n 2. View all customers. \n 3.Update a customer. \n 4.Delete a customer. \n 5.Quit.");
+  mongoose.connect(process.env.MONGODB_URI);
+  console.log("What would you like to do? Type the number of chosen action \n 1. Create a customer. \n 2. View all customers. \n 3. Update a customer. \n 4. Delete a customer. \n 5. Quit.");
   const userChoice = await prompt();
   if (userChoice === "1"){
     createCustomer();
-    setTimeout(userAction(), "3000");
   } else if (userChoice === "2"){
     viewCustomers();
-    setTimeout(userAction(), "3000");
   } else if (userChoice === "3"){
-
+    updateCustomer();
   } else if (userChoice == "4"){
-
+    deleteCustomer();
   } else if (userChoice === "5"){
     console.log("Exiting....");
-    await mongoose.disconnect();
+    await mongoose.connection.close();
   } else {
     console.log("This is not an option.");
     userAction();
